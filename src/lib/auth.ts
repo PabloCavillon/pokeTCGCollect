fba9c1c405e1c6050b918a32997655a997726b01
PabloCavillon <pabloa.cavillon@gmail.com>
2026-04-14 01:18:29 -0300

    fix auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,26 +1,17 @@
-import { db } from "@/db";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { users, accounts, sessions, verificationTokens } from "@/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: DrizzleAdapter(db, {
-    usersTable:              users,
-    accountsTable:           accounts,
-    sessionsTable:           sessions,
-    verificationTokensTable: verificationTokens,
-  }),
-  providers: [
-    Google({
-      clientId:     process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
+	providers: [
+		Google({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+	callbacks: {
+		session({ session, token }) {
+			if (token.sub) session.user.id = token.sub;
+			return session;
+		},
+	},
 });
